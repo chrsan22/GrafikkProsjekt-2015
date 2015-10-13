f3f90f1cf83ce2b25a24efd7d710fe620432a1d1
Christer Sandal <christer.ikt@gmail.com>
2015-10-13 12:28:28 +0200

    Insert Grass Floor

--- Quantum/scripts/mainLevel.js
+++ Quantum/scripts/mainLevel.js
@@ -8,13 +8,13 @@ var init = function() {
     var aspect = width/height;
     var fov = 45;
     var near = 0.1;
-    var far = 500;
+    var far = 5000;
     var canvas = document.getElementById("canvas");
 
     // Create renderer, set antialias to true if possible
     var renderer = new THREE.WebGLRenderer({
         canvas: canvas,
-        antialias: true
+        //antialias: true
     });
 
     // Create camera
@@ -25,7 +25,7 @@ var init = function() {
 
     // Add camera to scene
     scene.add(camera);
-    camera.position.z = 500;
+    camera.position.z = 1000;
 
     // Clear window to black and set size
     renderer.setClearColor(0x000000);
@@ -33,21 +33,29 @@ var init = function() {
 
     // Create Ground
     var groundTexture = THREE.ImageUtils.loadTexture('resources/texture_grass.jpg');
-    var groundGeometry = new THREE.BoxGeometry(200, 200, 0);
+    var groundGeometry = new THREE.PlaneGeometry(1000, 500, 100);
     var groundMaterial = new THREE.MeshBasicMaterial({ map: groundTexture});
     var ground = new THREE.Mesh(groundGeometry, groundMaterial);
     scene.add(ground);
+    rotateObject(ground, [-0.9,0.2,0.0]);
 
     // Light Point on Grass
     var pointLight = new THREE.PointLight(0xFFFFFF, 5);
-    ground.add(pointLight);
+    //ground.add(pointLight);
 
     // Render the scene
     function render() {
+        rotateObject(ground, [0.0,0.0,0.0]);
         renderer.render(scene, camera);
         window.requestAnimFrame(render);
     }
     render();
+};
+
+var rotateObject = function(object, rotation) {
+    object.rotation.x += rotation[0];
+    object.rotation.y += rotation[1];
+    object.rotation.z += rotation[2];
 };
 
 window.addEventListener('load', init);
