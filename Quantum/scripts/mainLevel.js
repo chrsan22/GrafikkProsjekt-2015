506b48376a471272a2aa23a4d7d33268c4d4e269
Christer Sandal <christer.ikt@gmail.com>
2015-10-13 11:57:26 +0200

    Starting Point!

--- Quantum/scripts/mainLevel.js
+++ Quantum/scripts/mainLevel.js
@@ -1,13 +1,14 @@
 /**
- * Created by Christer on 13/10/15.
+ * Created by oysmal on 29.09.15.
  */
+
 var init = function() {
     var width = window.innerWidth;
     var height = window.innerHeight;
-    var aspect = width / height;
+    var aspect = width/height;
     var fov = 45;
     var near = 0.1;
-    var far = 1000;
+    var far = 500;
     var canvas = document.getElementById("canvas");
 
     // Create renderer, set antialias to true if possible
@@ -22,17 +23,24 @@ var init = function() {
     // Create scene
     var scene = new THREE.Scene();
 
-    // Create atmospheric white light
-    var amb = new THREE.AmbientLight(0xFFFFFF);
-    scene.add(amb);
-
     // Add camera to scene
     scene.add(camera);
-    camera.position.z = 250;
+    camera.position.z = 500;
 
     // Clear window to black and set size
     renderer.setClearColor(0x000000);
     renderer.setSize(width, height);
+
+    // Create Ground
+    var groundTexture = THREE.ImageUtils.loadTexture('resources/texture_grass.jpg');
+    var groundGeometry = new THREE.BoxGeometry(200, 200, 0);
+    var groundMaterial = new THREE.MeshBasicMaterial({ map: groundTexture});
+    var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    scene.add(ground);
+
+    // Light Point on Grass
+    var pointLight = new THREE.PointLight(0xFFFFFF, 5);
+    ground.add(pointLight);
 
     // Render the scene
     function render() {
@@ -54,3 +62,4 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
         };
 })();
+
