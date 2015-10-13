0bc6f06070b339e70920d247d451f039e8b66063
Christer Sandal <christer.ikt@gmail.com>
2015-10-13 10:54:03 +0200

    Implement Scene

--- Quantum/scripts/mainLevel.js
+++ Quantum/scripts/mainLevel.js
@@ -1,3 +1,56 @@
 /**
  * Created by Christer on 13/10/15.
  */
+var init = function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var aspect = width / height;
+    var fov = 45;
+    var near = 0.1;
+    var far = 1000;
+    var canvas = document.getElementById("canvas");
+
+    // Create renderer, set antialias to true if possible
+    var renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        antialias: true
+    });
+
+    // Create camera
+    var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+    // Create scene
+    var scene = new THREE.Scene();
+
+    // Create atmospheric white light
+    var amb = new THREE.AmbientLight(0xFFFFFF);
+    scene.add(amb);
+
+    // Add camera to scene
+    scene.add(camera);
+    camera.position.z = 250;
+
+    // Clear window to black and set size
+    renderer.setClearColor(0x000000);
+    renderer.setSize(width, height);
+
+    // Render the scene
+    function render() {
+        renderer.render(scene, camera);
+        window.requestAnimFrame(render);
+    }
+    render();
+};
+
+window.addEventListener('load', init);
+
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
