cb3f5566b0de39dd684473934862eb9e8be3c7ce
Christer Sandal <christer.ikt@gmail.com>
2015-10-20 14:01:24 +0200

    Fixed Texture

--- Quantum/scripts/mainLevel.js
+++ Quantum/scripts/mainLevel.js
@@ -78,7 +78,7 @@ var init = function() {
     var heightMapGeometry = new HeightMapBufferGeometry(terrainData, worldWidth, worldDepth);   // Generate terrain geometry and mesh
     heightMapGeometry.scale(50*worldWidth, 1000, 50*worldDepth);    // Scale Geometry
 
-    texture = THREE.ImageUtils.loadTexture("resources/heightmap_11.png");   // Heightmap Texture
+    texture = THREE.ImageUtils.loadTexture("resources/texture_molten.jpg");   // Heightmap Texture
     ground = new HeightMapMesh( heightMapGeometry, new THREE.MeshPhongMaterial( { map: terrainTexture, map: texture } ) );
     ground.name = "terrain";
 
