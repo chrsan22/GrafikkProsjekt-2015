c499e1e49700b80148e94262d3d0d035386070c6
Butz85 <thomas.j.u@live.no>
2015-10-23 10:53:52 +0200

    no message

--- Quantum/scripts/mainLevelWithHeightmap.js
+++ Quantum/scripts/mainLevelWithHeightmap.js
@@ -9,10 +9,9 @@
 var init = function() {
 
     scene = new THREE.Scene(); // Scene
-    scene.fog = new THREE.FogExp2( 0xefd1b5, 0.0025 );
+    scene.fog = new THREE.FogExp2( 0xefd1b5, 0.0005 );
 
     var canvas = document.getElementById("canvas"); // Canvas
-    var createObject = new CreateObject(); // Contains functions to create usefulFunctions
     var createLight = new CreateLight(); // Contains functions to create light
     var heightMapFncs = new HeightMapFunctions(); // Contains functions used in the heightmap
 
