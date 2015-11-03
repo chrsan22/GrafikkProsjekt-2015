7988dd3897e4f46ff7885041bbea6277c9db588f
Tanuki <magbrimsholm@hotmail.com>
2015-11-03 13:56:25 +0100

    Opprettet vann med bevegelser

--- Quantum/scripts/mainLevelMag.js
+++ Quantum/scripts/mainLevelMag.js
@@ -4,6 +4,7 @@ var grassGroup = new THREE.Object3D();
 var tieMesh;
 var keyboard;
 var flightControlGroup = new THREE.Object3D();
+var water;
 
 var init = function () {
     var canvas = document.getElementById("canvas"); // Canvas
@@ -18,6 +19,7 @@ var init = function () {
         tieMesh = object;
         tieMesh.rotation.y = 180*(Math.PI/180);
         tieMesh.name = "tieMesh";
+        tieMesh.castShadow = true;
 
         flightControlGroup.add(camera);
         flightControlGroup.add(tieMesh);
@@ -42,7 +44,7 @@ var init = function () {
 
     renderer = cleanerMain.renderSettings(renderer);
     document.body.appendChild(renderer.domElement);   // Sets Size
-
+/*
     // Start of Grass testing
     var geometry = new THREE.PlaneBufferGeometry(100, 100);
     var groundUnderGrass = new THREE.CubeGeometry(100, 0, 100);
@@ -51,7 +53,7 @@ var init = function () {
         //map: THREE.ImageUtils.loadTexture( 'resources/textures/texture_snow.jpg' ), overdraw: false,
     });
     var meshing = new THREE.Mesh(groundUnderGrass, color);
-    scene.add(meshing);
+   scene.add(meshing);
     meshing.position.set(50, 0, 50);
 
     var texture2 = new THREE.CanvasTexture(generateTexture());
@@ -70,7 +72,7 @@ var init = function () {
 
         grassGroup.add(grassMesh);
     }
-
+*/
     function generateTexture() {
         var canvas = document.createElement('canvas');
         canvas.width = 1024;
@@ -99,16 +101,40 @@ var init = function () {
 
     var grid = new THREE.GridHelper(20000, 100); // Create Grid
     var skybox = createObject.skyBox("resources/skybox3/", "cube", "tCube", 20000, 30000, 20000)    // Create Skybox
-    var ground = createObject.heightMap("resources/textures/texture_snow.jpg", "heightmap", "terrain", 300, 20, 300, 50, 0, -150)    // Create Heightmap Ground
-
-    scene.add(grassGroup); // Adds Dynamic Grass to Scene
+    var ground = createObject.heightMap("resources/textures/texture_snow.jpg", "heightmap", "terrain", 300, 20, 300, 50, -2, -150)    // Create Heightmap Ground
+    ground.recieveShadow = true;
+    //scene.add(grassGroup); // Adds Dynamic Grass to Scene
     scene.children.reverse();   // Reverses the children in the opposite direction.
-    scene.add(grid);    // Adds Helping Grid for easy view
+    //scene.add(grid);    // Adds Helping Grid for easy view
     scene.add(skybox);  // Adds SkyBox to Scene
     scene.add(ground);  // Adds Heightmap Ground to Scene
     scene.add(ambientLight);    // Adds Ambiebt Light to Scene
     scene.add(lightPoint);  // Adds Light Point to Scene
 
+    // Create ocean
+    // Load textures
+    var waterNormals = new THREE.ImageUtils.loadTexture('resources/heightmaps/waternormals.jpg');
+    waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+
+    // Create the water effect
+    water = new THREE.Water(renderer, camera, scene, {
+        textureWidth: 2000,
+        textureHeight: 2000,
+        waterNormals: waterNormals,
+        alpha: 	1.0,
+        sunDirection: lightPoint.position.normalize(),
+        waterColor: 0x001e0f,
+        betaVersion: 0,
+        side: THREE.DoubleSide
+    });
+    var aMeshMirror = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(2000, 2000, 10, 10),
+        water.material
+    );
+
+    aMeshMirror.add(water);
+    aMeshMirror.rotation.x = - Math.PI * 0.5;
+    scene.add(aMeshMirror);
 
 
     function onWindowResize() {
@@ -124,23 +150,21 @@ var init = function () {
 function render() {
     var delta = clock.getDelta(); // seconds.
     controls.update(delta);   // Update Controls
+    water.material.uniforms.time.value += 1.0 / 60.0;
+    water.render();
     renderer.render(scene, camera); // Repeat Renderer
     window.requestAnimFrame(render);    // Banana
 
     var time = Date.now() / 6000;
-
+/*
     for (var i = 0, l = grassGroup.children.length; i < l; i++) {
         var Posmesh = grassGroup.children[i];
         Posmesh.position.x = 50 + Math.sin(time * 4) * i * i * 0.005;
         Posmesh.position.z = 50 + Math.cos(time * 6) * i * i * 0.005;
     }
-
-
-
-
-
+*/
      //TIE- control section
-   //  var tieMesh = scene.getObjectByName("tieMesh");
+    //  var tieMesh = scene.getObjectByName("tieMesh");
      var moveDistance = 50 * delta; // 50 pixels per second
      var rotateAngle = Math.PI / 2 * delta;   // pi/2 radians (90 degrees) per second
 
@@ -157,10 +181,10 @@ function render() {
      tieMesh.translateZ(moveDistance);
      }
      var rotation_matrix = new THREE.Matrix4().identity();
-     if (keyboard.pressed("y")) {
+     if (keyboard.pressed("i")) {
      tieMesh.rotateOnAxis(new THREE.Vector3(0, 0, 1), rotateAngle);
      }
-     if (keyboard.pressed("i")) {
+     if (keyboard.pressed("y")) {
      tieMesh.rotateOnAxis(new THREE.Vector3(0, 0, 1), -rotateAngle);
      }
     // camera.position.set(tieMesh.position.x,tieMesh.position.y+5,tieMesh.position.z+30);
@@ -180,3 +204,79 @@ window.requestAnimFrame = (function () {
         };
 })();
 
+/*******************
+    Ocean section
+  ******************/
+
+/*
+function initiateOcean(){
+var gsize = 512;
+var res = 1024;
+var gres = res / 2;
+var origx = -gsize / 2;
+var origz = -gsize / 2;
+ocean = new THREE.Ocean(renderer, this.camera, this.scene,
+    {
+        USE_HALF_FLOAT : hash === 'half-float',
+        INITIAL_SIZE : 256.0,
+        INITIAL_WIND : [10.0, 10.0],
+        INITIAL_CHOPPINESS : 1.5,
+        CLEAR_COLOR : [1.0, 1.0, 1.0, 0.0],
+        GEOMETRY_ORIGIN : [origx, origz],
+        SUN_DIRECTION : [-1.0, 1.0, 1.0],
+        OCEAN_COLOR: new THREE.Vector3(0.004, 0.016, 0.047),
+        SKY_COLOR: new THREE.Vector3(3.2, 9.6, 12.8),
+        EXPOSURE : 0.35,
+        GEOMETRY_RESOLUTION: gres,
+        GEOMETRY_SIZE : gsize,
+        RESOLUTION : res
+    });
+    ocean.materialOcean.uniforms.u_projectionMatrix = { type: "m4", value: camera.projectionMatrix };
+    ocean.materialOcean.uniforms.u_viewMatrix = { type: "m4", value: camera.matrixWorldInverse };
+    ocean.materialOcean.uniforms.u_cameraPosition = { type: "v3", value: camera.position };
+    scene.add(ocean.oceanMesh);
+
+var gui = new dat.GUI();
+var c1 = gui.add(ocean, "size",100, 5000);
+c1.onChange(function(v) {
+    this.object.size = v;
+    this.object.changed = true;
+});
+var c2 = gui.add(ocean, "choppiness", 0.1, 4);
+c2.onChange(function (v) {
+    this.object.choppiness = v;
+    this.object.changed = true;
+});
+var c3 = gui.add(ocean, "windX",-15, 15);
+c3.onChange(function (v) {
+    this.object.windX = v;
+    this.object.changed = true;
+});
+var c4 = gui.add(ocean, "windY", -15, 15);
+c4.onChange(function (v) {
+    this.object.windY = v;
+    this.object.changed = true;
+});
+var c5 = gui.add(ocean, "sunDirectionX", -1.0, 1.0);
+c5.onChange(function (v) {
+    this.object.sunDirectionX = v;
+    this.object.changed = true;
+});
+var c6 = gui.add(ocean, "sunDirectionY", -1.0, 1.0);
+c6.onChange(function (v) {
+    this.object.sunDirectionY = v;
+    this.object.changed = true;
+});
+var c7 = gui.add(this.ms_Ocean, "sunDirectionZ", -1.0, 1.0);
+c7.onChange(function (v) {
+    this.object.sunDirectionZ = v;
+    this.object.changed = true;
+});
+var c8 = gui.add(this.ms_Ocean, "exposure", 0.0, 0.5);
+c8.onChange(function (v) {
+    this.object.exposure = v;
+    this.object.changed = true;
+});
+
+}
+*/
